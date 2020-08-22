77f6dd071e08d7cbd6d6f7e902b0330b44c35428
Amir Hossein Azizzadeh <amirhossein_azizzadeh@yahoo.com>
2020-08-22 21:58:39 +0430

    '2Shahrivar'

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "c2d6e008de3835d7f462eeb53fed0d35",
 "/": "c2d6e008de3835d7f462eeb53fed0d35",
-"main.dart.js": "e584b222ddd9ab9b1d469218caf522f5",
+"main.dart.js": "cd72291f9b42a2828f104d3dcf967c4b",
 "manifest.json": "0897ffc8af9c46d658575c27aef7f579"
 };
 
